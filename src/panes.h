28ae039181c2c84ee94660c64452f84885876878
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-04-05 22:32:43 +0100

    Add missing include

--- src/panes.h
+++ src/panes.h
@@ -1,6 +1,8 @@
 #ifndef CRYSTAL_PANES
 #define CRYSTAL_PANES
 
+#include <adwaita.h>
+
 void add_new_pane(GtkWidget* paned_frame, GtkWidget* pane_content);
 void remove_last_pane(GtkWidget* paned_frame);
 
