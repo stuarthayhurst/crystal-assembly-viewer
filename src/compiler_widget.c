5d06df13fe9f37d777fd9929db1d988d04ac26e7
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-04-06 02:22:02 +0100

    Add a compiler output window and argument input

--- src/compiler_widget.c
+++ src/compiler_widget.c
@@ -2,24 +2,46 @@
 
 #include "compiler_widget.h"
 
-/* TODO:
- - Compiler arguments
- - Compiler selection
- - Output section
- - Disable input
- - Add sensitive toggle
-*/
-
 GtkWidget* create_compiler_widget() {
-  GtkWidget* label = gtk_label_new("Hi there");
-  gtk_widget_set_margin_start(label, 4);
-  gtk_widget_set_margin_end(label, 4);
-  gtk_widget_set_margin_top(label, 4);
-  gtk_widget_set_margin_bottom(label, 4);
-  gtk_widget_set_hexpand(label, TRUE);
-  gtk_widget_set_vexpand(label, TRUE);
+  //Vertical box for the config and output
+  GtkWidget* vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
 
-  return label;
+  //Horizontal box for the compiler config
+  GtkWidget* hbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+  gtk_box_append(GTK_BOX(vbox), hbox);
+
+  //Create a text entry for the compiler arguments
+  GtkWidget* compiler_arguments = gtk_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(compiler_arguments), "Compiler arguments, e.g. -O2");
+
+  //Put the entry in the horizontal box, and pad it
+  gtk_box_append(GTK_BOX(hbox), compiler_arguments);
+  gtk_widget_set_margin_start(compiler_arguments, 4);
+  gtk_widget_set_margin_end(compiler_arguments, 4);
+  gtk_widget_set_margin_top(compiler_arguments, 4);
+
+  //Create a frame for the output
+  GtkWidget* text_frame = gtk_frame_new(NULL);
+  gtk_box_append(GTK_BOX(vbox), text_frame);
+  gtk_widget_set_margin_start(text_frame, 4);
+  gtk_widget_set_margin_end(text_frame, 4);
+  gtk_widget_set_margin_bottom(text_frame, 4);
+
+  //Add a read-only text area to the frame
+  GtkWidget* text_view = gtk_text_view_new();
+  gtk_frame_set_child(GTK_FRAME(text_frame), text_view);
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), false);
+  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(text_view), false);
+
+  //Fill the area with the text box
+  gtk_widget_set_hexpand(text_view, TRUE);
+  gtk_widget_set_vexpand(text_view, TRUE);
+
+  //Set the text buffer to a placeholder
+  GtkTextBuffer* text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+  gtk_text_buffer_set_text(text_buffer, "...", -1);
+
+  return vbox;
 }
 
 void set_compiler_widget_compiling(GtkWidget* compiler_widget, bool compiling) {
